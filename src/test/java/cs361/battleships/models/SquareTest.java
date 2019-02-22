61d9e444786dbc39501b31fb71c689d62f15b527
strozzi1 <strozzij@oregonstate.edu>
2019-02-22 14:23:57 -0800

    started all test files

---
+++ src/test/java/cs361/battleships/models/SquareTest.java
@@ -0,0 +1,15 @@
+package cs361.battleships.models;
+
+import org.junit.Test;
+
+import javax.swing.plaf.basic.BasicOptionPaneUI;
+import cs361.battleships.ShipFactory;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+
+
+public class SquareTest {
+
+}
