50a52bedae3dfc2e4ccce2bddb748000d45570fb
Zach Kauffman <hyperion9823@gmail.com>
2019-02-22 17:44:29 -0800

    removed unecessary imports

--- src/test/java/cs361/battleships/models/SquareTest.java
+++ src/test/java/cs361/battleships/models/SquareTest.java
@@ -1,8 +1,7 @@
-package cs361.battleships;
+package cs361.battleships.models;
 
 import org.junit.Test;
 
-import javax.swing.plaf.basic.BasicOptionPaneUI;
 import cs361.battleships.ShipFactory;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
